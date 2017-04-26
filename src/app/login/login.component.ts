f725465a720b79d672018c9961c3452b6e9d6998
Stephan Grobler <stephan.grobler@gmail.com>
2017-04-26 16:30:45 +0200

    Add login method to component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from '../shared/user.service';
+
 
 @Component({
   templateUrl: 'login.component.html',
 })
 export class LoginComponent implements OnInit {
-  constructor() {  }
+  email: string;
+  password1: string;
+
+  constructor(private userSVC: UserService, private router: Router) {  }
 
   ngOnInit() {}
+
+  login(loginEmail: string, loginPassword: string){
+    this.userSVC.login(this.email, this.password1);
+    this.userSVC.verifyUser();
+  }
+
+  cancel(){
+    this.router.navigate(['/']);
+  }
 }
