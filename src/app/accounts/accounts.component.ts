fe1ac747735324c7092f7879a68394af5e74ae24
Stephan Grobler <stephan.grobler@gmail.com>
2017-04-30 10:40:09 +0200

    Added some components stuffs

---
+++ src/app/accounts/accounts.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'selector',
+  templateUrl: 'component-name.component.html',
+})
+export class nameComponent implements OnInit {
+  constructor() {  }
+
+  ngOnInit() {}
+}
