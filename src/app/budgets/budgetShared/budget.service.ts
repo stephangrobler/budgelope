526a2f5ab9bc5383e13418ef7c1a9a79ed3257c4
Stephan Grobler <stephan.grobler@gmail.com>
2017-05-01 18:56:18 +0200

    Add createBudget method to budget service

--- src/app/budgets/budgetShared/budget.service.ts
+++ src/app/budgets/budgetShared/budget.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
+import { Budget } from '../../shared/budget';
+
 
 @Injectable()
 export class BudgetService {
   constructor() {  }
 
-  
+  createBudget(budget: Budget){
+    let dbRef = firebase.database().ref('budgets');
+    let newBudget = dbRef.push();
+    newBudget.set({
+      name: budget.name,
+      userId: budget.userId,
+      id: newBudget.key
+    })
+  }
 
 }
