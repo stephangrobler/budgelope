784a6b430ced125a14a91c393ca0e5280b518dd1
Stephan Grobler <stephan.grobler@gmail.com>
2017-05-04 18:40:23 +0200

    Add firebase deploy scripts

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+import { Category } from '../shared/category';
+import { CategoryService} from '../core/category.service';
+import { UserService } from '../shared/user.service';
 
 @Component({
   templateUrl: 'categories.component.html',
 })
 
 export class CategoriesComponent implements OnInit {
-  constructor() {  }
+  userId: string;
+  categories: Category[];
 
-  ngOnInit() {}
+  constructor(
+    private userService: UserService
+  ) {  }
+
+  ngOnInit() {
+    this.userId = this.userService.authUser.uid;
+    this.getCategories();
+  }
+
+  getCategories(){
+    let dbRef = firebase.database().ref('categories/'+this.userId);
+    dbRef.once('value').then((snapshot) => {
+      let tmp: string[] = snapshot.val();
+      this.categories = Object.keys(tmp).map(key => tmp[key]);
+    });
+  }
 }
