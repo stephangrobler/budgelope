68bfa1e06c4f952b434a284eca6a239e06e65dcd
Stephan Grobler <stephan.grobler@gmail.com>
2017-05-13 22:39:27 +0200

    BudgetView - now working
    This is the best I am going to get it so we will just have to see
    how it fares with the massiveness of the updates that may start
    occurring. Hope firebase can cope.

--- src/app/budgets/budgetview/budgetview.component.ts
+++ src/app/budgets/budgetview/budgetview.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/take';
+import * as firebase from 'firebase';
 
 import { BudgetService } from '../../core/budget.service';
 import { UserService } from '../../shared/user.service';
@@ -15,28 +19,67 @@ export class BudgetviewComponent implements OnInit {
   allocations: FirebaseListObservable<any>;
   userId: string;
   activeBudget: any;
-  selectedMonth: string = '201704';
+  selectedMonth: string = '201707';
+  monthDisplay: Date;
 
   constructor(
-    private db:AngularFireDatabase,
+    private db: AngularFireDatabase,
     private budgetService: BudgetService,
     private userService: UserService
   ) {
     // this.activeBudget = this.budgetService.getActiveBudget();
     // this.userId = this.userService.authUser.uid;
-    this.activeBudget = {id: '-Kj4WoSIBP26dbPlEwj5'};
+    this.activeBudget = { id: '-Kj4WoSIBP26dbPlEwj5' };
     this.userId = '';
+    this.allocations = db.list('categoryAllocations/' + this.activeBudget.id + '/' + this.selectedMonth);
+    this.allocations.subscribe(snp => {
 
-    db.list('categories/' + this.activeBudget.id).subscribe((catSnap) => {
-      catSnap.forEach((cat) => {
-        let allRef = this.db.object('categoryAllocations/' + this.activeBudget.id +'/'+this.selectedMonth+'/' + cat.$key);
-        cat.allocations = allRef;
-      });
-      this.categories = catSnap;
-    });
+      if (snp.length == 0){
+        console.log('creating new allocations records for ' + this.selectedMonth);
+        // get the categories list and push new allocations to the list.
+        db.list('categories/'+this.activeBudget.id).subscribe(catSnapshots => {
+          catSnapshots.forEach(catSnapshot => {
+            this.allocations.update(catSnapshot.$key, {
+              planned: 0,
+              actual: 0,
+              balance: 0,
+              name: catSnapshot.name
+            });
+          });
+        });
+      }
+    })
   }
 
   ngOnInit() {
   }
 
+  alert(key) {
+    console.log('key for', key);
+  }
+
+  focus(item) {
+    console.log('focus for', item);
+  }
+
+  blur(item) {
+
+    if (item.planned) {
+      // cat.subscribe(catSnap => {
+      //   val = catSnap.val();
+      // });
+      //   if (!val.balance) {
+      //     val.balance = 0;
+      //   }
+      //   console.log(val);
+      //   console.log("OldBalance", val.balance);
+      //   val.balance += (item.Planned - val.old.Planned);
+      //   console.log("NewBalance", val.balance);
+      //   // check to see if the price is updated or the price is from zero
+      //   // catRef.update({ "balance": val.balance });
+      //   console.log(val);
+
+      this.allocations.update(item.$key, { "planned": item.planned });
+    }
+  }
 }
