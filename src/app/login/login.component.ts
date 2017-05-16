404f88a34e65b10895c53afe56bcf47b967216b6
Stephan Grobler <stephan.grobler@gmail.com>
2017-05-16 21:48:46 +0200

    One page home
    Added some home page stuffs.
    Added google login.
    Added hide show of logged in user navigations.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 import { UserService } from '../shared/user.service';
 
@@ -10,14 +13,29 @@ import { UserService } from '../shared/user.service';
 export class LoginComponent implements OnInit {
   email: string;
   password1: string;
+  user: Observable<firebase.User>;
 
-  constructor(private userSVC: UserService, private router: Router) {  }
+  constructor(
+    private userSVC: UserService,
+    private router: Router,
+    public afAuth: AngularFireAuth
+  ) {
+    this.user = afAuth.authState;
+  }
 
   ngOnInit() {}
 
   login(loginEmail: string, loginPassword: string){
     this.userSVC.login(this.email, this.password1);
     this.userSVC.verifyUser();
+  }
+
+  googleLogin() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((user) => {
+      console.log(user.user.providerData[0]);
+      
+      this.router.navigate(['/']);
+    });
   }
 
   signUp(){
