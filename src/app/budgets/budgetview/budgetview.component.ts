a1660baa5c63fe6139b2d79888e8f420732cc3b3
Stephan Grobler <stephan.grobler@gmail.com>
2017-05-17 22:41:41 +0200

    BudgetView seem to have cracked the totals
    Added totalIncome, totalExpense, totalAvailable and totalBudgeted.
    Tallies seem correct. Will do more testing, almost there where I
    can invite beta testers :)

--- src/app/budgets/budgetview/budgetview.component.ts
+++ src/app/budgets/budgetview/budgetview.component.ts
@@ -23,6 +23,11 @@ export class BudgetviewComponent implements OnInit {
   selectedMonth: any = moment();
   monthDisplay: Date;
 
+  totalIncome: number = 0;
+  totalExpense: number = 0;
+  totalBudgeted: number = 0;
+  totalAvailable: number = 0;
+
   constructor(
     private db: AngularFireDatabase,
     private budgetService: BudgetService,
@@ -35,7 +40,19 @@ export class BudgetviewComponent implements OnInit {
     this.userId = '';
     this.allocations = db.list('categoryAllocations/' + this.activeBudget.id + '/' + this.selectedMonth.format("YYYYMM"));
     this.allocations.subscribe(snp => {
-
+      this.totalExpense = 0;
+      this.totalBudgeted = 0;
+      this.totalIncome = 0;
+      this.totalAvailable = 0;
+      snp.forEach(val => {
+        this.totalBudgeted += parseFloat(val.planned);
+        if (val.type == "income"){
+          this.totalIncome += parseFloat(val.actual);
+        } else {
+          this.totalExpense += parseFloat(val.actual);
+        }
+      });
+      this.totalAvailable = this.totalIncome - this.totalBudgeted;
       if (snp.length == 0){
         console.log('creating new allocations records for ' + this.selectedMonth);
         // get the categories list and push new allocations to the list.
@@ -51,6 +68,57 @@ export class BudgetviewComponent implements OnInit {
         });
       }
     })
+    let start = moment().date(1);
+    let end = moment();
+    console.log('moment-start', start.valueOf());
+    console.log('moment-end', end.valueOf());
+    // get all the transactions for this month
+    //
+    /* Uncomment to run transaction sync to actual month stuffs 
+    let tranlist = db.list('transactions/'+this.activeBudget.id);
+    db.list('transactions/'+this.activeBudget.id).take(1).subscribe(tracks => {
+      let running: number = 0;
+      let accObj = {};
+      let catObj = {};
+      let allObj = {};
+      tracks.forEach(track => {
+        if ("expense" == track.type){
+          track.amount = -Math.abs(track.amount);
+        } else {
+          track.amount = Math.abs(track.amount);
+        }
+        running += parseFloat(track.amount);
+        console.log('amount', track.$key, parseFloat(track.amount));
+        console.log('running', running);
+        tranlist.update(track.$key, {amount: track.amount});
+        // account update object
+
+        let accRef = 'accounts/'+this.activeBudget.id+'/'+track.accountId+'/balance';
+        let catRef = 'categories/'+this.activeBudget.id+'/'+track.categoryId+'/balance';
+        let allRef = 'categoryAllocations/'+this.activeBudget.id+'/'+this.selectedMonth.format('YYYYMM')+'/'+track.categoryId+'/actual';
+        if (!accObj[accRef]){
+          accObj[accRef] = 0;
+        }
+        if (!catObj[catRef]){
+          catObj[catRef] = 0;
+        }
+        if (!allObj[allRef]){
+          allObj[allRef] = 0;
+        }
+        allObj[allRef] = allObj[allRef] + parseFloat(track.amount);
+        accObj[accRef] = accObj[accRef] + parseFloat(track.amount);
+        catObj[catRef] = catObj[catRef] + parseFloat(track.amount);
+
+      });
+      console.log(accObj);
+      console.log(catObj);
+      console.log(allObj);
+
+      db.object('/').update(accObj);
+      db.object('/').update(catObj);
+      db.object('/').update(allObj);
+    });
+    /**/
   }
 
   ngOnInit() {
@@ -59,6 +127,10 @@ export class BudgetviewComponent implements OnInit {
 
   alert(key) {
     console.log('key for', key);
+  }
+
+  calculateBalance(actual, planned){
+    return parseFloat(planned) + parseFloat(actual);
   }
 
   focus(item) {
