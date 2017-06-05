f0cfa98eea617318ca5badc8a345e107ad1d4214
Stephan Grobler <stephan.grobler@gmail.com>
2017-06-05 22:24:10 +0200

    Move cloud functions to the web services
    Moved the Cloud Functions back to the web interface as it is
    hitting the limitations on the server :( till it is paid for,
    the client will have to ensure that it is done correctly :(

--- src/app/core/category.service.ts
+++ src/app/core/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Category } from '../shared/category';
 import { AngularFireDatabase } from 'angularfire2/database';
 import * as firebase from 'firebase';
+import * as moment from 'moment';
 
 @Injectable()
 export class CategoryService {
@@ -13,20 +14,63 @@ export class CategoryService {
   createCategory(budgetId: string, category: Category){
     let dbRef = this.db.list('categories/' + budgetId);
     let newCat = dbRef.push(category);
+    let categoryId = newCat.key;
     // create a allocation
+    // current allocation
+    let currentDate = new Date();
+    let month = moment().format('YYYYMM');
+    let nextMonth = moment().add(1, 'months').format("YYYYMM");
+    let catData = category;
+    let allocData = {
+      "actual": 0,
+      "balance": 0,
+      "planned": 0,
+      "previousBalance": 0,
+      "name": catData.name,
+      "parent": catData.parent,
+      "sortingOrder": catData.sortingOrder,
+      "type": catData.type,
+    }
+    let currentAllocationMonthRef = '/allocations/' + budgetId + '/' + month;
+    let nextAllocationMonthRef = '/allocations/' + budgetId + '/' + nextMonth;
+    return Promise.all([
+      admin.database().ref(currentAllocationMonthRef).child(categoryId).set(allocData),
+      admin.database().ref(nextAllocationMonthRef).child(categoryId).set(allocData),
+      admin.database().ref('/categoryAllocations/' + budgetId + '/' + categoryId).child(month).set(true),
+      admin.database().ref('/categoryAllocations/' + budgetId + '/' + categoryId).child(nextMonth).set(true)
+    ]).then(() => {
+      console.log('Created ' + catData.name + ' successfully!');
+    });
 
   }
 
   updateCategory(budgetId: string, category: Category){
     // update main category
     let dbRef = this.db.object('categories/' + budgetId + '/' + category.$key);
+    let categoryId = category.$key;
     console.log(category);
     // update allocations
-    let allRef = this.db.list('categoryAllocations/'+ budgetId + '/' + category.$key);
-    allRef.subscribe(all => {
-      console.log(all);
-    })
-    // update transactions
+    let updateObj = {};
+
+    // get all allocations
+    let allocationsRef = '/categoryAllocations/' + budgetId + '/' + categoryId;
+    // update the allocations
+    let allocationLocations = admin.database().ref(allocationsRef).once('value').then(results => {
+      let allResults = results.val();
+      // update allocations
+
+      Object.keys(allResults).forEach(month => {
+        // push all different items to the object
+        let refAll = '/allocations/' + budgetId + '/' + month + '/' + categoryId;
+        updateObj[refAll + '/sortingOrder'] = category.sortingOrder;
+        updateObj[refAll + '/name'] = category.name;
+        updateObj[refAll + '/parent'] = category.parent;
+        updateObj[refAll + '/type'] = category.type;
+      });
+      return admin.database().ref('/').update(updateObj).then(() => {
+        console.log('Update Category ' + categoryId + ':' + category.name + ' complete.');
+      });
+    });
   }
 
   deleteCategory(budgetId: string, category: Category){
--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -31,16 +31,16 @@ export class UserService implements CanActivate {
     return false;
   }
 
-  register(email: string, password: string){
+  register(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
-      .catch(function(error){
+      .catch(function(error) {
         alert(`${error.message} Please try again!`);
       });
   }
 
   verifyUser() {
     this.authUser = firebase.auth().currentUser;
-    if (this.authUser){
+    if (this.authUser) {
       this.loggedInUser = this.authUser.email;
       this.userLoggedIn = true;
     }
@@ -48,17 +48,364 @@ export class UserService implements CanActivate {
 
   login(loginEmail: string, loginPassword: string) {
     firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword)
-      .catch(function(error){
+      .catch(function(error) {
         alert(`${error.message} Unable to login. Try again.`);
       })
   }
 
-  logout(){
+  logout() {
     this.userLoggedIn = false;
-    firebase.auth().signOut().then(function(){
+    firebase.auth().signOut().then(function() {
       alert(`Logged out!`);
-    }, function(error){
+    }, function(error) {
       alert(`${error.message} Unable to logout. Try again!`);
+    });
+  }
+
+  setupProfile(uid: string) {
+    if (!uid){
+      return;
+    }
+
+    let budgetRef = firebase.database().ref('/budgets').push();
+    let budgetKey = budgetRef.key;
+    // create a budget
+    budgetRef.update({
+      active: true,
+      balance: 0,
+      name: "Starting Budget",
+      owner: uid
+    });
+
+    // create a profile and set active budget
+    let profileRef = firebase.database().ref('/users/' + uid).set({
+      activeBudget: budgetKey,
+      registered: "",
+      subscription: "trial"
+    });
+
+    // create all default categories for the budget
+    let categories = [
+      {
+        "name": "Giving",
+        "parent": ""
+      }, {
+        "name": "Tithing",
+        "parent": "Giving"
+      }, {
+        "name": "Offerings",
+        "parent": "Giving"
+      }, {
+        "name": "Charities",
+        "parent": "Giving"
+      }, {
+        "name": "Specific Needs",
+        "parent": "Giving"
+      }, {
+        "name": "Food",
+        "parent": ""
+      }, {
+        "name": "Groceries",
+        "parent": "Food"
+      }, {
+        "name": "Restaurants",
+        "parent": "Food"
+      }, {
+        "name": "Pet food/Treats",
+        "parent": "Food"
+      }, {
+        "name": "Shelter",
+        "parent": ""
+      }, {
+        "name": "Mortgage",
+        "parent": "Shelter"
+      }, {
+        "name": "Rent",
+        "parent": "Shelter"
+      }, {
+        "name": "Property Taxes",
+        "parent": "Shelter"
+      }, {
+        "name": "Household Repairs",
+        "parent": "Shelter"
+      }, {
+        "name": "Utilities",
+        "parent": ""
+      }, {
+        "name": "Electricity",
+        "parent": "Utilities"
+      }, {
+        "name": "Water",
+        "parent": "Utilities"
+      }, {
+        "name": "Heating",
+        "parent": "Utilities"
+      }, {
+        "name": "Garbage",
+        "parent": "Utilities"
+      }, {
+        "name": "Phones",
+        "parent": "Utilities"
+      }, {
+        "name": "Cable",
+        "parent": "Utilities"
+      }, {
+        "name": "Internet",
+        "parent": "Utilities"
+      }, {
+        "name": "Clothing",
+        "parent": ""
+      }, {
+        "name": "Childrens Clothing",
+        "parent": "Clothing"
+      }, {
+        "name": "Adults Clothing",
+        "parent": "Clothing"
+      }, {
+        "name": "Transportation",
+        "parent": ""
+      }, {
+        "name": "Fuel",
+        "parent": "Transportation"
+      }, {
+        "name": "Tires",
+        "parent": "Transportation"
+      }, {
+        "name": "Oil Changes",
+        "parent": "Transportation"
+      }, {
+        "name": "Maintenance",
+        "parent": "Transportation"
+      }, {
+        "name": "Parking Fees",
+        "parent": "Transportation"
+      }, {
+        "name": "Repairs",
+        "parent": "Transportation"
+      }, {
+        "name": "Licence Fees",
+        "parent": "Transportation"
+      }, {
+        "name": "Vehicle Replacement",
+        "parent": "Transportation"
+      }, {
+        "name": "Medical",
+        "parent": ""
+      }, {
+        "name": "Primary Care",
+        "parent": "Medical"
+      }, {
+        "name": "Dental Care",
+        "parent": "Medical"
+      }, {
+        "name": "Speciality Care",
+        "parent": "Medical"
+      }, {
+        "name": "Medications",
+        "parent": "Medical"
+      }, {
+        "name": "Medical Devices",
+        "parent": "Medical"
+      }, {
+        "name": "Insurance",
+        "parent": ""
+      }, {
+        "name": "Health Insurance",
+        "parent": "Insurance"
+      }, {
+        "name": "Homeowners Insurance",
+        "parent": "Insurance"
+      }, {
+        "name": "Renters Insurance",
+        "parent": "Insurance"
+      }, {
+        "name": "Auto Insurance",
+        "parent": "Insurance"
+      }, {
+        "name": "Life Insurance",
+        "parent": "Insurance"
+      }, {
+        "name": "Disability Insurance",
+        "parent": "Insurance"
+      }, {
+        "name": "Identity Insurance",
+        "parent": "Insurance"
+      }, {
+        "name": "Longterm Care Insurance",
+        "parent": "Insurance"
+      }, {
+        "name": "Household Items",
+        "parent": ""
+      }, {
+        "name": "Toiletries",
+        "parent": "Household Items"
+      }, {
+        "name": "Laundry Detergent",
+        "parent": "Household Items"
+      }, {
+        "name": "Dishwasher Detergent",
+        "parent": "Household Items"
+      }, {
+        "name": "Cleaning Supplies",
+        "parent": "Household Items"
+      }, {
+        "name": "Personal",
+        "parent": ""
+      }, {
+        "name": "Gym Memberships",
+        "parent": "Personal"
+      }, {
+        "name": "Hair Cuts",
+        "parent": "Personal"
+      }, {
+        "name": "Salon Services",
+        "parent": "Personal"
+      }, {
+        "name": "Cosmeticts",
+        "parent": "Personal"
+      }, {
+        "name": "Babysitter",
+        "parent": "Personal"
+      }, {
+        "name": "Child Support",
+        "parent": "Personal"
+      }, {
+        "name": "Alimony",
+        "parent": "Personal"
+      }, {
+        "name": "Subscriptions",
+        "parent": "Personal"
+      }, {
+        "name": "Debt Reduction",
+        "parent": ""
+      }, {
+        "name": "Mortgage",
+        "parent": "Debt Reduction"
+      }, {
+        "name": "Credit Card",
+        "parent": "Debt Reduction"
+      }, {
+        "name": "Personal Loan",
+        "parent": "Debt Reduction"
+      }, {
+        "name": "Student Loan",
+        "parent": "Debt Reduction"
+      }, {
+        "name": "Retirement",
+        "parent": ""
+      }, {
+        "name": "Financial Planning",
+        "parent": "Retirement"
+      }, {
+        "name": "Investing",
+        "parent": "Retirement"
+      }, {
+        "name": "Education",
+        "parent": ""
+      }, {
+        "name": "Financial Coaching",
+        "parent": "Education"
+      }, {
+        "name": "Childrens College",
+        "parent": "Education"
+      }, {
+        "name": "Your College",
+        "parent": "Education"
+      }, {
+        "name": "School Supplies",
+        "parent": "Education"
+      }, {
+        "name": "Books",
+        "parent": "Education"
+      }, {
+        "name": "Conferences",
+        "parent": "Education"
+      }, {
+        "name": "Savings",
+        "parent": ""
+      }, {
+        "name": "Emergency Fund",
+        "parent": "Savings"
+      }, {
+        "name": "Hill and Valley Fund",
+        "parent": "Savings"
+      }, {
+        "name": "Other Savings",
+        "parent": "Savings"
+      }, {
+        "name": "Gifts",
+        "parent": ""
+      }, {
+        "name": "Birthday",
+        "parent": "Gifts"
+      }, {
+        "name": "Anniversary",
+        "parent": "Gifts"
+      }, {
+        "name": "Wedding",
+        "parent": "Gifts"
+      }, {
+        "name": "Christmas",
+        "parent": "Gifts"
+      }, {
+        "name": "Special Occasion",
+        "parent": "Gifts"
+      }, {
+        "name": "Fun Money",
+        "parent": ""
+      }, {
+        "name": "Entertainment",
+        "parent": "Fun Money"
+      }, {
+        "name": "Games",
+        "parent": "Fun Money"
+      }, {
+        "name": "Eating Out",
+        "parent": "Fun Money"
+      }, {
+        "name": "Spontaneous Giving",
+        "parent": "Fun Money"
+      }, {
+        "name": "Vacations",
+        "parent": "Fun Money"
+      }, {
+        "name": "Subscriptions",
+        "parent": "Fun Money"
+      },];
+
+    let baseCategory = {
+      "name": "",
+      "parent": "",
+      "parentId": "",
+      "balance": 0,
+      "hidden": false,
+      "sortingOrder": "",
+      "type": "expense"
+    }
+
+    let catListRef = firebase.database().ref('/categories/' + budgetKey);
+    let parentCount = 0;
+    let parentKey = "";
+    let childCount = 0;
+    categories.forEach(category => {
+      let catRef = catListRef.push();
+      let catKey = catRef.key;
+      baseCategory.name = category.name;
+      baseCategory.parent = category.parent;
+
+      if (category.parent == "") {
+        parentCount++;
+        baseCategory.sortingOrder = ('000' + parentCount).slice(-3);
+        parentKey = catKey;
+        childCount = 0;
+      } else {
+        childCount++;
+        baseCategory.sortingOrder = ('000' + parentCount).slice(-3);
+        baseCategory.sortingOrder += ':' + ('000' + childCount).slice(-3);
+        baseCategory.parentId = parentKey;
+      }
+
+      catRef.set(baseCategory);
     });
   }
 
