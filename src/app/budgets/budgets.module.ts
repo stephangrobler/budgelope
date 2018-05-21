0fc348ebbf477dae7beddc2083749b2ec5f4afe5
Stephan Grobler <stephan.grobler@gmail.com>
2018-05-21 22:20:15 +0200

    remove unnessecary import for budget service

--- src/app/budgets/budgets.module.ts
+++ src/app/budgets/budgets.module.ts
@@ -6,7 +6,6 @@ import { DragulaModule } from 'ng2-dragula';
 import { BudgetsComponent } from './budgets.component';
 import { BudgetComponent } from './budget/budget.component';
 import { BudgetListComponent } from './budgetlist/budgetlist.component';
-import { BudgetService } from '../core/budget.service';
 import { SharedModule } from '../shared/shared.module';
 import { BudgetviewComponent } from './budgetview/budgetview.component';
 import { AppRoutingModule } from '../shared/app-routing.module';
