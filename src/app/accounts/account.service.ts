4e89a439e1a026a9e24c582193d65f0369a6fa67
Stephan Grobler <stephan.grobler@gmail.com>
2018-08-19 07:53:40 +0200

    updated account balance update to use a transaction

--- src/app/accounts/account.service.ts
+++ src/app/accounts/account.service.ts
@@ -4,12 +4,13 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Account } from '../shared/account';
 import { Budget } from '../shared/budget';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { TransactionService } from '../transactions/transaction.service';
+import { FirebaseApp } from 'angularfire2';
 
 @Injectable()
 export class AccountService {
-  constructor(private db: AngularFirestore) {}
+  constructor(private db: AngularFirestore, private fb: FirebaseApp) {}
 
   getAccounts(budgetId: string): Observable<Account[]> {
     return this.db
@@ -34,18 +35,35 @@ export class AccountService {
     // });
   }
 
-  updateAccount(update: Account, budgetId: string) {
-    const dbRef = 'budgets/' + budgetId + '/accounts/' + update.id;
+  /**
+   * Updates the balance of an account in a transaction
+   *
+   * @param accountId The id of the account to be updated
+   * @param budgetId The budget of which the account needs to be updated
+   * @param amount The amount that the account needs to be updated with
+   */
+  updateAccountBalance(accountId: string, budgetId: string, amount: number) {
+    const dbRef = 'budgets/' + budgetId + '/accounts/' + accountId;
+    const docRef = this.db.doc(dbRef).ref;
 
-    this.db
-      .doc(dbRef)
-      .update({
-        name: update.name,
-        balance: update.balance
-      })
-      .then(result => {
-        console.log('accountt updated', result);
-      });
+    this.fb.firestore().runTransaction(transaction => {
+      return transaction.get(docRef).then(
+        account => {
+          const newBalance = account.data().balance + amount;
+          transaction.update(docRef, { balance: newBalance });
+        },
+        error => {
+          console.log(
+            'There was an error updating the balance of the account: ' +
+              accountId +
+              ' - ' +
+              budgetId +
+              ' - ',
+            error
+          );
+        }
+      );
+    });
   }
 
   removeAccount(delAccount: Account) {
