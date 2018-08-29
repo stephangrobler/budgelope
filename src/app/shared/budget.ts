db7c336da1b37d3354981b12ba83cac5bfc8bb18
Stephan Grobler <stephan.grobler@britehouse.co.za>
2018-08-29 11:10:39 +0200

    Added type check to budget allocations type

--- src/app/shared/budget.ts
+++ src/app/shared/budget.ts
@@ -1,24 +1,22 @@
 export class Budget {
-
   public name: string;
   public start: Date;
   public active: boolean;
   public balance: number;
-  public allocations: object;
+  public allocations?: { [yearMonth: string]: { income: number; expense: number } };
   public sharedWith: string[];
   public userId?: string;
   public id?: string;
 
-
-  constructor(budgetData? : any ) {
-    if(budgetData){
-      this.id = budgetData.id ? budgetData.id : null ;
-      this.name = budgetData.name ? budgetData.name : null ;
-      this.start = budgetData.start ? budgetData.start : null ;
-      this.active = budgetData.active ? budgetData.active : null ;
-      this.balance = budgetData.balance ? budgetData.balance : null ;
-      this.allocations = budgetData.allocations ? budgetData.allocations : null ;
-      this.userId = budgetData.userId ? budgetData.userId : null ;
+  constructor(budgetData?: any) {
+    if (budgetData) {
+      this.id = budgetData.id ? budgetData.id : null;
+      this.name = budgetData.name ? budgetData.name : null;
+      this.start = budgetData.start ? budgetData.start : null;
+      this.active = budgetData.active ? budgetData.active : null;
+      this.balance = budgetData.balance ? budgetData.balance : null;
+      this.allocations = budgetData.allocations ? budgetData.allocations : null;
+      this.userId = budgetData.userId ? budgetData.userId : null;
     }
   }
 }
