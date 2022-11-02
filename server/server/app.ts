437238c1e75388759b61ea05ddba0742c264c8a5
Stephan Grobler <stephan.grobler@gmail.com>
2022-11-02 09:11:33 +0200

    dockerized

--- server/server/app.ts
+++ server/server/app.ts
@@ -1,32 +1,32 @@
-import dotenv from "dotenv";
-const dotenvResult = dotenv.config({ path: "./server/.env" });
+import dotenv from 'dotenv';
+const dotenvResult = dotenv.config({ path: './server/.env' });
 if (dotenvResult.error) {
-  throw dotenvResult.error;
+    throw dotenvResult.error;
 }
-import express from "express";
-import * as http from "http";
+import express from 'express';
+import * as http from 'http';
 
-import * as winston from "winston";
-import * as expressWinston from "express-winston";
-import cors from "cors";
-import { CommonRoutesConfig } from "./common/common.routes.config";
-import { UsersRoutes } from "./users/users.routes.config";
-import debug from "debug";
-import { TransactionRoutes } from "./transactions/transactions.routes.config";
-import { BudgetRoutes } from "./budgets/budgets.routes.config";
-import { AccountRoutes } from "./accounts/accounts.routes.config";
-import { PayeeRoutes } from "./payees/payees.routes.config";
-import { CategoriesRoutes } from "./categories/categories.routes.config";
-import { AuthRoutes } from "./auth/auth.routes.config";
-import { CategoryGroupsRoutes } from "./categories_groups/category_groups.routes.config";
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import cors from 'cors';
+import { CommonRoutesConfig } from './common/common.routes.config';
+import { UsersRoutes } from './users/users.routes.config';
+import debug from 'debug';
+import { TransactionRoutes } from './transactions/transactions.routes.config';
+import { BudgetRoutes } from './budgets/budgets.routes.config';
+import { AccountRoutes } from './accounts/accounts.routes.config';
+import { PayeeRoutes } from './payees/payees.routes.config';
+import { CategoriesRoutes } from './categories/categories.routes.config';
+import { AuthRoutes } from './auth/auth.routes.config';
+import { CategoryGroupsRoutes } from './categories_groups/category_groups.routes.config';
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 5005;
 const routes: Array<CommonRoutesConfig> = [];
-const debugLog: debug.IDebugger = debug("app");
+const debugLog: debug.IDebugger = debug('app');
 
-app.use(express.static(process.cwd()+"/web/dist/"));
+app.use(express.static(process.cwd() + '/web/dist/'));
 
 // here we are adding middleware to parse all incoming requests as JSON
 app.use(express.json());
@@ -37,16 +37,16 @@ app.use(cors());
 // here we are preparing the expressWinston logging middleware configuration,
 // which will automatically log all HTTP requests handled by Express.js
 const loggerOptions: expressWinston.LoggerOptions = {
-  transports: [new winston.transports.Console()],
-  format: winston.format.combine(
-    winston.format.json(),
-    winston.format.prettyPrint(),
-    winston.format.colorize({ all: true })
-  ),
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize({ all: true })
+    ),
 };
 
 if (!process.env.DEBUG) {
-  loggerOptions.meta = false; // when not debugging, log requests as one-liners
+    loggerOptions.meta = false; // when not debugging, log requests as one-liners
 }
 
 // initialize the logger with the above configuration
@@ -65,14 +65,15 @@ routes.push(new AuthRoutes(app));
 
 // this is a simple route to make sure everything is working properly
 const runningMessage = `Server running at http://localhost:${port}`;
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.status(200).send(runningMessage);res.sendFile(process.cwd()+"/web/dist/index.html")
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.status(200).send(runningMessage);
+    res.sendFile(process.cwd() + '/web/dist/index.html');
 });
 
 // huh?
 server.listen(port, () => {
-  debugLog(`Server running at http://localhost:${port}`);
-  routes.forEach((route: CommonRoutesConfig) => {
-    debugLog(`Routes configured for ${route.getName()}`);
-  });
+    debugLog(`Server running at http://localhost:${port}`);
+    routes.forEach((route: CommonRoutesConfig) => {
+        debugLog(`Routes configured for ${route.getName()}`);
+    });
 });
